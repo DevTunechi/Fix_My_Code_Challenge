75603df837921460d1563079560eac98682fce7d
DevTunechi <tunjidare2@yahoo.com>
2024-03-13 16:25:34 -0700

    JavaScript - Ruby - Python - Readme

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+if (process.argv.length !== 3) {
+    process.stderr.write("Missing or extra argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
+}
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size) || size < 1) {
+    process.stderr.write("Invalid size\n");
+    process.stderr.write("Size must be a positive integer\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+        row += "#";
+    }
+    console.log(row);
+}
